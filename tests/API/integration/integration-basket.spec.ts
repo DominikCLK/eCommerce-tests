99198e9db5ec5ea010717ec6e9de6c61fae0de85
DominikCLK <dominik.calak@gmail.com>
2025-01-20 23:38:36 +0100

    test git

--- tests/API/integration/integration-basket.spec.ts
+++ tests/API/integration/integration-basket.spec.ts
@@ -5,6 +5,8 @@ import { API_URL } from 'config/env.config';
 
 const buildUrl = (endpoint: string): string => `${API_URL}${endpoint}`;
 
+
+
 test.describe.configure({ mode: 'serial' });
 test.describe('Basket operations integration tests @API-integration', () => {
   const expectedAddItemResponse = 'item added or updated';
