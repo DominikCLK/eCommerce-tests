b43e59ef88f21898c36f5e22e0c1c2ff39aff459
DominikCLK <dominik.calak@gmail.com>
2025-01-23 22:11:01 +0100

    test: tests of navbar for logged users

--- src/factories/testCases/navbarTestCases.ts
+++ src/factories/testCases/navbarTestCases.ts
@@ -1,13 +1,22 @@
-export const navbarTestCases = [
+export const navbarTabsTestCases = [
   { navbarTab: 'home', url: '/' },
   { navbarTab: 'contact', url: 'contact' },
   { navbarTab: 'sign-in', url: 'auth/login' },
 ];
 
-export const categoriesTestCases = [
+export const categoriesTabsTestCases = [
   { categories: 'hand-tools', url: 'category/hand-tools' },
   { categories: 'power-tools', url: 'category/power-tools' },
   { categories: 'other', url: 'category/other' },
   { categories: 'special-tools', url: 'category/special-tools' },
   { categories: 'rentals', url: 'rentals' },
+];
+
+export const accountTabsTestCases = [
+  { accountTab: 'my-account', url: 'account' },
+  { accountTab: 'my-favorites', url: 'account/favorites' },
+  { accountTab: 'my-profile', url: 'account/profile' },
+  { accountTab: 'my-invoices', url: 'account/invoices' },
+  { accountTab: 'my-messages', url: 'account/messages' },
+  { accountTab: 'sign-out', url: '/' },
 ];
